943f056ee030b06a22c3137bdf7f127c3090d5d6
wjl54 <1910354889@qq.com>
2025-09-23 19:13:29 +0800

    新建项目

---
+++ ticktes-common/src/main/java/com/longlong/ticktescommon/TicktesCommonApplication.java
@@ -0,0 +1,13 @@
+package com.longlong.ticktescommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicktesCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicktesCommonApplication.class, args);
+	}
+
+}
---
+++ ticktes-common/src/test/java/com/longlong/ticktescommon/TicktesCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longlong.ticktescommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicktesCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
