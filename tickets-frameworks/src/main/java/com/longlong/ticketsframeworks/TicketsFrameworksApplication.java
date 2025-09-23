0796122cf3ae13922d42ab9c978e9630ea3a69ab
wjl54 <1910354889@qq.com>
2025-09-23 23:47:58 +0800

    提交

---
+++ tickets-frameworks/src/main/java/com/longlong/ticketsframeworks/TicketsFrameworksApplication.java
@@ -0,0 +1,13 @@
+package com.longlong.ticketsframeworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsFrameworksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketsFrameworksApplication.class, args);
+    }
+
+}
---
+++ tickets-frameworks/src/test/java/com/longlong/ticketsframeworks/TicketsFrameworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longlong.ticketsframeworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsFrameworksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
