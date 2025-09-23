da16237375acaebca3a93500f7a02acbccb3b731
wjl54 <1910354889@qq.com>
2025-09-23 23:06:55 +0800

    UserService持久层Service创建

---
+++ tickets-service/user-service/src/main/java/com/longlong/user/service/UserService.java
@@ -0,0 +1,8 @@
+package com.longlong.user.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.longlong.user.pojo.entity.UserEntity;
+
+public interface UserService extends IService<UserEntity> {
+
+}
---
+++ tickets-service/user-service/src/main/java/com/longlong/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.longlong.user.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.longlong.user.mapper.UserMapper;
+import com.longlong.user.pojo.entity.UserEntity;
+import com.longlong.user.service.UserService;
+
+public class UserServiceImpl extends ServiceImpl<UserMapper, UserEntity> implements UserService {
+}
