e3b0838802065db7f9aa2b81bb7f513b268028e4
wjl54 <1910354889@qq.com>
2025-09-23 23:40:44 +0800

    缝缝补补

---
+++ tickets-commons/src/main/java/com/longlong/ticketscommons/TicketsCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.longlong.ticketscommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsCommonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketsCommonsApplication.class, args);
+    }
+
+}
---
+++ tickets-commons/src/test/java/com/longlong/ticketscommons/TicketsCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longlong.ticketscommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsCommonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
